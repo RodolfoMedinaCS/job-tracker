b503c7c43b87329d951674f544f00ce49999158a
Rodolfo Medina <medinar8197@student.laccd.edu>
2026-05-23 01:20:38 -0700

    add AddApplication form redesign with sections    and fix CSS module scoping

--- frontend/src/pages/AddApplication/AddApplication.jsx
+++ frontend/src/pages/AddApplication/AddApplication.jsx
@@ -1,8 +1,16 @@
-import Navbar from "../../components/NavBar/NavBar.jsx";
 import styles from "./AddApplication.module.css"
 import {useState} from "react";
 import {useNavigate} from "react-router-dom";
 import {useLocation} from "react-router-dom";
+import { FaBuilding } from "react-icons/fa";
+import { BsBriefcaseFill } from "react-icons/bs";
+import { FaLocationArrow } from "react-icons/fa6";
+import { FaRegUser } from "react-icons/fa";
+import { CgNotes } from "react-icons/cg";
+
+
+
+
 
 
 function AddApplication(){
@@ -108,54 +116,129 @@ function AddApplication(){
             )}
 
             <div className={styles.pageWrapper}>
-                <div className={styles.appOptions}>
-                    <button  onClick={backToDash} >Cancel</button>
-                    <button onClick={handleSubmit}>Add + </button>
-                </div>
 
-                <div className={styles.contentBody}>
-                    <div className={styles.formGroup}>
-                        <div className={styles.companyName}>
-                            <label>Company: </label>
-                            <input type="text" value={formData.company} onChange={(e) =>
-                            setFormData({...formData, company: e.target.value})} />
+                <div className={styles.formContainer}>
+
+                    {/*Company block*/}
+                    <div className={styles.companySection}>
+                        <label><FaBuilding/> COMPANY</label>
+                        <div className={styles.companyTop}>
+                            <div className={styles.companyName}>
+                                <label>Company name</label>
+                                <input className={styles.inputField} placeholder="e.g. Google" type="text" value={formData.company} onChange={(e) =>
+                                    setFormData({...formData, company: e.target.value})} />
+                            </div>
+                            <div className={styles.companyWebsite}>
+                                <label>Company Website</label>
+                                <input className={styles.inputField}  type="text" placeholder="careers.google.com"/>
+                            </div>
                         </div>
 
-                        <div className={styles.titleName}>
-                            <label>Job Title: </label>
-                            <input  type="text" value={formData.jobTitle} onChange={(e) =>
-                            setFormData({...formData, jobTitle: e.target.value })} />
-                        </div>
-
-                        <div className={styles.jobStatus}>
-                            <label>Job Status: </label>
-                            <select value={formData.status} onChange={(e) =>
-                            setFormData({...formData, status: e.target.value})} >
-                                <option value="">Select Status</option>
-                                <option value="SUBMITTED">Submitted</option>
-                                <option value="UNDER_REVIEW">Under Review</option>
-                                <option value="INTERVIEW_SCHEDULED">Interview Scheduled</option>
-                                <option value="OFFER_EXTENDED">Offer Extended</option>
-                                <option value="REJECTED">Rejected</option>
-                                <option value="HIRED">Hired</option>
-                            </select>
-                        </div>
-
-                        <div className={styles.jobNotes}>
-                            <label>Job Notes: </label>
-                            <textarea value={formData.notes} onChange={(e) =>
-                            setFormData({...formData, notes: e.target.value})} >
-                            </textarea>
-                        </div>
-
-                        <div className={styles.jobDate}>
-                            <label>Date Applied: </label>
-                            <input type="date" value={formData.dateApplied} onChange={(e) =>
-                                setFormData({...formData, dateApplied: e.target.value})} />
+                        <div className={styles.companyBot}>
+                            <div className={styles.companyLocation}>
+                                <label>Location</label>
+                                <input className={styles.inputField}  type="text" placeholder="Mountain View, CA"/>
+                            </div>
+                            <div className={styles.companyWorkType}>
+                                <label>Work Type</label>
+                                <select className={styles.selectField} >
+                                    <option>remote</option>
+                                    <option>in-person</option>
+                                </select>
+                            </div>
                         </div>
                     </div>
 
+                    {/*Position Block*/}
+                    <div className={styles.positionSection}>
+                        <label><BsBriefcaseFill></BsBriefcaseFill> POSITION</label>
+                        <div className={styles.posTop}>
+                            <div className={styles.jobTitle}>
+                                <label>Job Title: </label>
+                                <input placeholder="Software Engineer Intern" className={styles.inputField} type="text" value={formData.jobTitle} onChange={(e) =>
+                                    setFormData({...formData, jobTitle: e.target.value })} />
+                            </div>
+                            <div className={styles.jobType}>
+                                <label>Job Type</label>
+                                <select className={styles.selectField} >
+                                    <option>Internship</option>
+                                    <option>Senior</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div className={styles.posBot}>
+                            <div className={styles.posUrl}>
+                                <label>Job posting url</label>
+                                <input placeholder="https://jobs.lever.co/..." className={styles.inputFieldSpecial}  type="text"/>
+                            </div>
+                        </div>
+                    </div>
 
+                    {/*Application Details Block*/}
+                    <div className={styles.appDetSection}>
+                        <label><FaLocationArrow></FaLocationArrow> APPLICATION DETAILS</label>
+                        <div className={styles.appDevTop}>
+                            <div className={styles.dateApplied}>
+                                <label>Date Applied</label>
+                                <input className={styles.inputField}  type="date" value={formData.dateApplied} onChange={(e) =>
+                                    setFormData({...formData, dateApplied: e.target.value})} />
+                            </div>
+                            <div className={styles.status}>
+                                <label>Job Status: </label>
+                                <select className={styles.selectField} value={formData.status} onChange={(e) =>
+                                    setFormData({...formData, status: e.target.value})} >
+                                    <option value="">Select Status</option>
+                                    <option value="SUBMITTED">Submitted</option>
+                                    <option value="UNDER_REVIEW">Under Review</option>
+                                    <option value="INTERVIEW_SCHEDULED">Interview Scheduled</option>
+                                    <option value="OFFER_EXTENDED">Offer Extended</option>
+                                    <option value="REJECTED">Rejected</option>
+                                    <option value="HIRED">Hired</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div className={styles.appDevBot}>
+                            <div className={styles.salMin}>
+                                <label>Salary (min)</label>
+                                <input className={styles.inputField}  type="text" placeholder="$ 120,000"/>
+                            </div>
+                            <div className={styles.salMax}>
+                                <label>Salary (max)</label>
+                                <input className={styles.inputField}  type="text" placeholder="$ 150,000"/>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/*Recruiter Contact block*/}
+                    <div className={styles.recConSection}>
+                        <label><FaRegUser></FaRegUser> Recruiter Contact</label>
+                        <div className={styles.recDet}>
+                            <div className={styles.recName}>
+                                <label>Recruiter Name</label>
+                                <input className={styles.inputField}  type="text" placeholder="Jane Doe"/>
+                            </div>
+                            <div className={styles.recEmail}>
+                                <label>Recruiter Email</label>
+                                <input  className={styles.inputField} type="email" placeholder="jane@google.com"/>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* Notes Block */}
+                    <div className={styles.notesBlock}>
+                        <label><CgNotes></CgNotes> NOTES</label>
+                        <div className={styles.notes}>
+                            <label>Notes</label>
+                            <textarea placeholder="Anything worth remembering — interview prep, who referred you..." className={styles.textAreaSpecial} value={formData.notes} onChange={(e) =>
+                                setFormData({...formData, notes: e.target.value})} >
+                            </textarea>
+                        </div>
+                    </div>
+
+                    <div className={styles.appOptions}>
+                        <button className={styles.addBttns}  onClick={backToDash} >Cancel</button>
+                        <button className={styles.addBttns} onClick={handleSubmit}>Add + </button>
+                    </div>
                 </div>
             </div>
         </>
